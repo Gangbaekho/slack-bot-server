4410394c3ddbaacd5688818458aa218f61039d78
kimjinsoo <roorooro9933@gmail.com>
2021-09-09 00:00:41 +0900

    erase yml

--- src/main/java/com/nuitblanche/slackbotserver/controller/SlackBotController.java
+++ src/main/java/com/nuitblanche/slackbotserver/controller/SlackBotController.java
@@ -5,14 +5,12 @@ import com.nuitblanche.slackbotserver.response.ListResult;
 import com.nuitblanche.slackbotserver.service.ResponseService;
 import com.nuitblanche.slackbotserver.service.SlackBotService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RequiredArgsConstructor
+@CrossOrigin("*")
 @RestController
 public class SlackBotController {
 
