4d6448508111636e08204453b6e2c2d8f996ea52
kimjinsoo <roorooro9933@gmail.com>
2021-09-08 17:18:19 +0900

    init

---
+++ src/main/java/com/nuitblanche/slackbotserver/SlackBotServerApplication.java
@@ -0,0 +1,13 @@
+package com.nuitblanche.slackbotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackBotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackBotServerApplication.class, args);
+    }
+
+}
